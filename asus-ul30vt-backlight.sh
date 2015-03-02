d8231504b70d0a88fd370f27711745a3f69339fc
someone2b <someone2b@users.noreply.github.com>
2015-03-01 20:52:25 -0600

    Updated Comments

--- asus-ul30vt-backlight.sh
+++ asus-ul30vt-backlight.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-#logger "hit hotkey script"
-
-# this directory is a symlink on my machine:
+# Directory where backlight exists
+# strangely, for the ul30vt (dual video cards) 
+# even if the nvidia card is in use, backlight
+# control only works through the intel_backlight
 KEYS_DIR=/sys/class/backlight/intel_backlight
 
 test -d $KEYS_DIR || exit 0
@@ -10,7 +11,7 @@ test -d $KEYS_DIR || exit 0
 MIN=0
 MAX=$(cat $KEYS_DIR/max_brightness)
 VAL=$(cat $KEYS_DIR/brightness)
-STEP=$((MAX / 8))
+STEP=$((MAX / 8)) #the visual indicator in xubuntu seems to have 8 steps
 
 if [ "$1" = down ]; then
 	VAL=$((VAL-STEP))
